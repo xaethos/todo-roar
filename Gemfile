2354869ec956ea5f1eadaa54cc733608becf43af
Diego García <diego@xaethos.net>
2013-09-08 18:51:51 +0800

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+
+gem 'rails', '4.0.0'
+
+gem 'pg'
+
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+
+gem 'roar-rails'
+
+group :development, :test do
+  gem 'awesome_print'
+  gem 'pry'
+end
+
+# Use debugger
+# gem 'debugger', group: [:development, :test]
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TodoRoar::Application.config.secret_key_base = 'c058def43cab9c8c17f2e8e1e5f29e0a1b905815a87a0d5ffb9a08c3fca364cfa18c63926af4c7f1d1214617657663c93d5fae169e830dc429d6ac895b08486f'
