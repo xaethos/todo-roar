3012c86af748d1de94d1d673936b5553e45c34b6
Diego García <diego@xaethos.net>
2013-09-08 21:13:31 +0800

    todos/index

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
 gem 'turbolinks'
 
+gem 'haml'
 gem 'roar-rails'
 
 group :development, :test do
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ TodoRoar::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'todos#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -13,6 +14,7 @@ TodoRoar::Application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :todos, only: [:index]
 
   # Example resource route with options:
   #   resources :products do
